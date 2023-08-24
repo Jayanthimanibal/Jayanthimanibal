a065262263599082811a6409c55eb41e8c05215d
Jayanthimanibal <142036264+Jayanthimanibal@users.noreply.github.com>
2023-08-24 20:43:39 +0530

    Add files via upload

---
+++ code.php
@@ -0,0 +1,8 @@
+<?php
+$x1="Jayanthi";
+$x2="Manibal";
+$x3="CSE Department";
+$x4="Adhiparasakthi Engineering College";
+$x5="Melmaruvathur";
+echo"Hi, I am" .$x1.$x2."from".$x3. "of" .$x4."," .$x5;
+?>   
